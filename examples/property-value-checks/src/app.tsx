345b760462ffc6d036495cd720eb1235f3206628
Tim Buschtoens <tbuschto@eclipsesource.com>
2019-04-16 17:30:23 +0200

    Add example property-value-checks

---
+++ examples/property-value-checks/src/app.tsx
@@ -0,0 +1,38 @@
+import { Button, contentView, StackComposite, TextInput, TextView } from 'tabris';
+import { Person } from './Person';
+
+const person = new Person();
+
+contentView.append(
+  <StackComposite layoutData='fill' alignment='stretchX' padding={12} spacing={12}>
+    <TextInput text='X' message='Name (at least 2 chars)' onAccept={changeName}/>
+    <Button onSelect={changeName}>Change Name</Button>
+    <TextInput text='-5' message='Age (positive number)' onAccept={changeAge}/>
+    <Button onSelect={changeAge}>Change Age</Button>
+    <TextView font={{size: 18}}/>
+  </StackComposite>
+);
+
+function changeName() {
+  try {
+    person.name = $(TextInput).first().text;
+    log(`Name is now ${person.name}`);
+  } catch (ex) {
+    log(ex.message);
+  }
+}
+
+function changeAge() {
+  try {
+    person.age = parseInt($(TextInput).last().text, 10);
+    log(`Age is now ${person.age}`);
+  } catch (ex) {
+    log(ex.message);
+  }
+}
+
+function log(message: string) {
+  // tslint:disable-next-line: no-console
+  console.log(message);
+  $(TextView).only().text = message;
+}
